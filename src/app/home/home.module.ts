f83229e34d838ef634bf9deb8872fcdb59919a0d
notsuhas <suhas@marax.in>
2020-11-03 03:50:58 +0530

    create side nav

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { Routes, RouterModule } from '@angular/router';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, SidenavComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class HomeModule {}
